9cbef1a47f2f62289569436fb6371e6fc0672132
emine2021 <81431972+emine2021@users.noreply.github.com>
2021-05-01 00:56:48 +0300

    mesaj

--- src/gitProject/Myclass01.java
+++ src/gitProject/Myclass01.java
@@ -20,7 +20,7 @@ public class Myclass01 {
 		System.out.println("branch olusturduk-feature branch"); //branch i cmd de değistirince kod feature da gozukuyor master da gozukmuyor
 		System.out.println("baska degisiklik");
 		System.out.println(" daha once master da degılmisim:) master branch e satır ekledik");
-		
+		System.out.println("feature a yeni satir");
 
 
 		
