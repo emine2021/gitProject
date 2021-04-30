c3b72eca454c85b758becc65270bbf50752d7a99
emine2021 <81431972+emine2021@users.noreply.github.com>
2021-04-30 21:50:18 +0300

    2.gün degisiklik

--- src/gitProject/Myclass01.java
+++ src/gitProject/Myclass01.java
@@ -3,7 +3,7 @@ package gitProject;
 public class Myclass01 {
 
 	public static void main(String[] args) {
-		System.out.println("Birinci gün");
+		System.out.println("BÝrinci gün");
 		System.out.println("ikinci commit");
 
 	}
