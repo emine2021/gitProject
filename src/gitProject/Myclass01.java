ef417d75a9fb956bb6e24c9a3630e4a28ae6a012
emine2021 <81431972+emine2021@users.noreply.github.com>
2021-04-30 16:09:55 +0300

    ikinci commit

--- src/gitProject/Myclass01.java
+++ src/gitProject/Myclass01.java
@@ -3,7 +3,8 @@ package gitProject;
 public class Myclass01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Birinci gün");
+		System.out.println("ikinci commit");
 
 	}
 
